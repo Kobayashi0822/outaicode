ab10d8475ea409d085b68efa22a1ec15bfae4388
Kobayashi0822 <49467240+Kobayashi0822@users.noreply.github.com>
2021-03-10 22:34:35 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function copy() {
+    var targetCode = document.getElementById("code"); 
+    targetCode.select();                              
+    document.execCommand("Copy");                     
+}
+
+function copy2() {
+    var targetCode = document.getElementById("code2"); 
+    targetCode.select();                              
+    document.execCommand("Copy");                     
+}
